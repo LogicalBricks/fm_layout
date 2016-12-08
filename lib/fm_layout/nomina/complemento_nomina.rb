27ae3b0233a9dccbd0261ec59c1e4b28984556f4
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2016-12-08 14:41:57 -0600

    Add field to complemento_nomina

--- lib/fm_layout/nomina/complemento_nomina.rb
+++ lib/fm_layout/nomina/complemento_nomina.rb
@@ -21,6 +21,7 @@ module FmLayout
           'FechaInicialPago'       => 'fecha_inicial_de_pago',
           'FechaFinalPago'         => 'fecha_final_de_pago',
           'NumDiasPagados'         => 'dias_pagados',
+          'TotalPercepciones'      => 'total_percepciones',
           'Departamento'           => 'departamento',
           'CLABE'                  => 'clabe',
           'Banco'                  => 'banco',
