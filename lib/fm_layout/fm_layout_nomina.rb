2b2626aae9496c915fa06fd188479b5ca570723a
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2017-01-02 13:03:36 -0600

    Remove domicilio_fical and expedido of fm_layout_nomina

--- lib/fm_layout/fm_layout_nomina.rb
+++ lib/fm_layout/fm_layout_nomina.rb
@@ -1,7 +1,6 @@
 require 'fm_layout/datos_adicionales'
 require 'fm_layout/emisor'
 require 'fm_layout/receptor'
-require 'fm_layout/domicilio'
 require 'fm_layout/concepto'
 require 'fm_layout/impuesto_trasladado'
 require 'fm_layout/impuesto_trasladado_local'
@@ -53,33 +52,6 @@ module FmLayout
         yield(@receptor)
       else
         @receptor
-      end
-    end
-
-    def domicilio_fiscal
-      @domicilio_fiscal ||= Domicilio.new('DomicilioFiscal','=')
-      if block_given?
-        yield(@domicilio_fiscal)
-      else
-        @domicilio_fiscal
-      end
-    end
-
-    def domicilio
-      @domicilio ||= Domicilio.new('Domicilio','=')
-      if block_given?
-        yield(@domicilio)
-      else
-        @domicilio
-      end
-    end
-
-    def expedido_en
-      @expedido_en ||= Domicilio.new('ExpedidoEn','=')
-      if block_given?
-        yield(@expedido_en)
-      else
-        @expedido_en
       end
     end
 
@@ -143,7 +115,7 @@ module FmLayout
     end
 
     def to_s
-      salida = @recibo_nomina.to_s + @datos_adicionales.to_s + @emisor.to_s + @domicilio_fiscal.to_s + @expedido_en.to_s + @receptor.to_s + @domicilio.to_s
+      salida = @recibo_nomina.to_s + @datos_adicionales.to_s + @emisor.to_s + @receptor.to_s
       salida += @conceptos.map(&:to_s).reduce(:+).to_s
       salida += @impuestos_trasladados.map(&:to_s).reduce(:+).to_s
       salida += @impuestos_retenidos.map(&:to_s).reduce(:+).to_s
@@ -157,10 +129,8 @@ module FmLayout
       recibo_nomina.to_h
         .merge(@datos_adicionales.to_h)
         .merge(@emisor.to_h)
-        .merge(@domicilio_fiscal.to_h)
         .merge(@expedido_en.to_h)
         .merge(@receptor.to_h)
-        .merge(@domicilio.to_h)
         .merge(obtener_hash_conceptos)
         .merge(obtener_hash_traslados)
         .merge(obtener_hash_retenciones)
